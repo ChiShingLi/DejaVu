a68ac0c4593a48f082cf3b67c6bda1e0dd085d94
cli16 <cli16@mail.wou.edu>
2023-01-28 14:11:55 -0800

    Added signing in as 'Demo User' feature for project demonstration purposes

--- client/src/components/mainPageSection/login/Login.js
+++ client/src/components/mainPageSection/login/Login.js
@@ -25,6 +25,11 @@ const Login = () => {
         password: ""
     });
 
+    const [demoLoginObj, setDemoLoginObj] = useState({
+        username: "demo",
+        password: process.env.REACT_APP_DEMO_USER_PASSWORD
+    });
+
     //user login details object
     const handleChange = (e) => {
         setLoginObj({ ...loginObj, [e.target.name]: e.target.value })
@@ -46,12 +51,27 @@ const Login = () => {
         setLoading(false);
     }
 
+    //handle submit button click
+    const handleDemoSubmit = async () => {
+        const result = await API_userLogin(demoLoginObj);
+        if (result.status === true) {
+            localStorage.setItem("token", result.token);
+            dispatch(updateUser(result.userDetails)); //store into redux userReducer
+            navigate("/home");
+            setLoading(false);
+        } else if (result.status === false) {
+            console.log(result);
+            { result.message.response.status === 401 ? showErrorNoti("Incorrect Username or Password", "Please try again.") : showErrorNoti("Internal Server Error", "Please try again.") }
+        }
+    }
+
     return (
         <div className={theme === "light" ? "loginSection" : "loginSection-dark"}>
             <h1>Sign In</h1>
             <input type="text" name="username" className="loginInput" placeholder="Username" onChange={handleChange} onBlur={formatInput} />
             <input type="password" name="password" className="loginInput" placeholder="Password" onChange={handleChange} onBlur={formatInput} />
             <button className="button login-btn" onClick={handleSubmit}>{loading ? <PulseLoader size={10} color={"white"} /> : "Log In"}</button>
+            <button className="button login-btn demoLogin-btn" onClick={handleDemoSubmit}>{loading ? <PulseLoader size={10} color={"white"} /> : "Log In as Demo User"}</button>
         </div>
     )
 }
